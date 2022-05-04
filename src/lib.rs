03234d1ad47b579d907a6a4fe8e984e0c9d92034
Guilherme Neubaner <guilherme.neubaner@gmail.com>
2022-05-04 20:27:57 -0300

    implement ArcWake trait from futures crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,43 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+use std::{sync::Arc, task::{Waker, RawWakerVTable, RawWaker}, mem::{self, ManuallyDrop}};
+
+pub trait ArcWake : Send + Sync {
+    fn wake_by_ref(self: &Arc<Self>);
+    fn wake(self: Arc<Self>) {
+        Self::wake_by_ref(&self)
     }
+}
+
+fn waker_vtable<W: ArcWake>() -> &'static RawWakerVTable {
+    &RawWakerVTable::new(
+        clone_arc_raw::<W>,
+        wake_arc_raw::<W>,
+        wake_arc_by_ref_raw::<W>,
+        drop_arc_raw::<W>
+    )
+}
+
+unsafe fn clone_arc_raw<W: ArcWake>(data: *const ()) -> RawWaker {
+    let arc = Arc::from_raw(data.cast::<W>());
+    // Increase the reference counting
+    mem::forget(arc.clone());
+    RawWaker::new(data, waker_vtable::<W>())
+}
+
+unsafe fn wake_arc_raw<W: ArcWake>(data: *const ()) {
+    let arc = Arc::from_raw(data.cast::<W>());
+    ArcWake::wake(arc)
+}
+
+unsafe fn wake_arc_by_ref_raw<W: ArcWake>(data: *const ()) {
+    let arc = ManuallyDrop::new(Arc::from_raw(data.cast::<W>()));
+    ArcWake::wake_by_ref(&arc);
+}
+
+unsafe fn drop_arc_raw<W: ArcWake>(data: *const ()) {
+    drop(Arc::from_raw(data.cast::<W>()));
+}
+
+pub fn waker<W: ArcWake>(arc: Arc<W>) -> Waker {
+    let ptr = Arc::into_raw(arc).cast::<()>();
+    unsafe { Waker::from_raw(RawWaker::new(ptr, waker_vtable::<W>())) }
 }
